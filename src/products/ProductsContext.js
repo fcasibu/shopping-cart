11702ea37c8f56c28edd8767a211f357dfef5f45
nevz <75290989+nevz9@users.noreply.github.com>
2022-04-19 21:56:21 +0800

    Add filtereCart
    Naming can be better on this

--- src/products/ProductsContext.js
+++ src/products/ProductsContext.js
@@ -3,6 +3,7 @@ import React from "react";
 const ProductsContext = React.createContext({
   items: [],
   cart: [],
+  filteredCart: [],
   addToCart: () => {},
   removeFromCart: () => {},
   openCart: () => {},
